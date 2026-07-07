206cad9474350d7ec2187ec24280ac5c8c2bb4dc
pbfoto-cpu <pbfoto@gmail.com>
2026-07-07 15:39:18 +0900

    Initial Xcode project scaffold

---
+++ MetaEdit/MetaEditApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetaEditApp.swift
+//  MetaEdit
+//
+//  Created by pb on 2026/07/07.
+//
+
+import SwiftUI
+
+@main
+struct MetaEditApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
